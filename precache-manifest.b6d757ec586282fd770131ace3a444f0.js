9b45db05b22a9d946dcd95c2c64453e99bd489a5
Aizat Faiz <aizat.faiz@gmail.com>
2020-03-12 21:25:40 +0800

    Updates

---
+++ precache-manifest.b6d757ec586282fd770131ace3a444f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a7884f313a9292728e7ba0b60a78ef",
+    "url": "/error-budget/index.html"
+  },
+  {
+    "revision": "69eaacdf13a7c75f389c",
+    "url": "/error-budget/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "edf23a851ba4dae11ead",
+    "url": "/error-budget/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "69eaacdf13a7c75f389c",
+    "url": "/error-budget/static/js/2.8378e093.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/error-budget/static/js/2.8378e093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf23a851ba4dae11ead",
+    "url": "/error-budget/static/js/main.597d165d.chunk.js"
+  },
+  {
+    "revision": "fa8f36b98667760892df",
+    "url": "/error-budget/static/js/runtime-main.48796dde.js"
+  }
+]);
